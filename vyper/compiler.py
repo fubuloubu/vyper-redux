59637d978f5ec44fefc8dafc5293162ae31141ba
Bryant Eisenbach <fubuloubu@gmail.com>
2020-01-25 01:53:28 -0500

    bug: Forgot python newline bug

--- vyper/compiler.py
+++ vyper/compiler.py
@@ -6,6 +6,6 @@ from .grammar import (
 )
 
 def compile_code(src: str):
-    tree = grammar.parse(src)
+    tree = grammar.parse(src + '\n')  # Python newline bug
     ast = ast_parse(tree)
     import pdb; pdb.set_trace()
